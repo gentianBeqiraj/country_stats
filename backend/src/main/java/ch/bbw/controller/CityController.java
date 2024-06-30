60d45d6420306993dabb9fba136421497cf57343
Gentian Beqiraj <105867457+gentianBeqiraj@users.noreply.github.com>
2024-06-30 16:04:00 +0200

    update CityController: use CityService to get filtered cities

--- backend/src/main/java/ch/bbw/controller/CityController.java
+++ backend/src/main/java/ch/bbw/controller/CityController.java
@@ -1,59 +1,42 @@
 package ch.bbw.controller;
 
 import ch.bbw.dtos.CityResponse;
-import ch.bbw.dtos.CountryResponse;
-import ch.bbw.util.APIClient;
+import ch.bbw.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/cities")
 public class CityController {
 
-  final String BASE_URL = "https://countriesnow.space/api/v0.1";
-  final Integer TIMEOUT = 10;
-  final APIClient client = new APIClient(BASE_URL, TIMEOUT);
+  private final CityService cityService;
+
+  @Autowired
+  public CityController(CityService cityService) {
+    this.cityService = cityService;
+  }
 
   @GetMapping("/")
   public String index() {
     return "index";
   }
 
-  private CountryResponse getCountryResponse() {
-    final String endpoint = "countries/population/cities/";
-
-    Map<String, String> headers = new HashMap<>();
-    headers.put("Content-Type", "application/json");
-
-    CountryResponse countryResponse = new CountryResponse();
-
-    try {
-      countryResponse = client.get(endpoint, headers, CountryResponse.class);
-    } catch (IOException | InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    return countryResponse;
-  }
-
   @GetMapping
   public String getCities(@RequestParam("country") String country, Model model) {
-    CountryResponse countryResponse = getCountryResponse();
-
-    List<CityResponse> filteredCities = countryResponse.getData().stream()
-        .filter(cityResponse -> country.equalsIgnoreCase(cityResponse.getCountry()))
-        .toList();
-
-    model.addAttribute("country", country);
-    model.addAttribute("cities", filteredCities);
-    return "countries";
+    try {
+      List<CityResponse> filteredCities = cityService.getCitiesByCountry(country);
+      model.addAttribute("country", country);
+      model.addAttribute("cities", filteredCities);
+      return "countries";
+    } catch (Exception e) {
+      model.addAttribute("error", "Unable to fetch cities at this time.");
+      return "error";
+    }
   }
 }
