2f67c21fdf712f0012263916fc135d2a1b5c205e
Gentian Beqiraj <105867457+gentianBeqiraj@users.noreply.github.com>
2024-06-30 18:14:47 +0200

    update CityService: implemented functional sorting

--- backend/src/main/java/ch/bbw/service/CityService.java
+++ backend/src/main/java/ch/bbw/service/CityService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,12 +23,35 @@ public class CityService {
     this.apiClient = new APIClient("https://countriesnow.space/api/v0.1", 10);
   }
 
-  public List<CityResponse> getCitiesByCountry(String country) throws IOException, InterruptedException {
-    ApiResponse response = getApiResponse();
+  public List<CityResponse> getCitiesByCountry(String country, String sortOrder)
+      throws IOException, InterruptedException {
 
-    return response.getData().stream()
+    List<CityResponse> cities = getApiResponse().getData().stream()
         .filter(cityResponse -> country.equalsIgnoreCase(cityResponse.getCountry()))
         .collect(Collectors.toList());
+
+    switch (sortOrder) {
+      case "nameAsc":
+        cities.sort(Comparator.comparing(CityResponse::getCity));
+        break;
+      case "nameDesc":
+        cities.sort(Comparator.comparing(CityResponse::getCity).reversed());
+        break;
+      case "populationAsc":
+        cities.sort(Comparator.comparing(city -> city.getPopulationCounts().getFirst().getValue()));
+        break;
+      case "populationDesc":
+        cities.sort(Comparator.comparing((CityResponse city) -> city.getPopulationCounts().getFirst().getValue()).reversed());
+        break;
+      case "yearAsc":
+        cities.sort(Comparator.comparing(city -> city.getPopulationCounts().getFirst().getYear()));
+        break;
+      case "yearDesc":
+        cities.sort(Comparator.comparing((CityResponse city) -> city.getPopulationCounts().getFirst().getYear()).reversed());
+        break;
+    }
+
+    return cities;
   }
 
   private ApiResponse getApiResponse() throws IOException, InterruptedException {
