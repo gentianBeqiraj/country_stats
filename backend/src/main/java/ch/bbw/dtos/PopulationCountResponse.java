ec6cf439654235f280dff6d78c46df5da5aece66
Gentian Beqiraj <105867457+gentianBeqiraj@users.noreply.github.com>
2024-07-03 13:58:08 +0200

    update PopulationCountResponse: use custom Json Deserializers

--- backend/src/main/java/ch/bbw/dtos/PopulationCountResponse.java
+++ backend/src/main/java/ch/bbw/dtos/PopulationCountResponse.java
@@ -1,11 +1,19 @@
 package ch.bbw.dtos;
 
+import ch.bbw.util.deserializers.StringToLongDeserializer;
+import ch.bbw.util.deserializers.StringToIntDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
 @Data
 public class PopulationCountResponse {
-  String year;
-  String value;
+
+  @JsonDeserialize(using = StringToIntDeserializer.class)
+  Integer year;
+
+  @JsonDeserialize(using = StringToLongDeserializer.class)
+  Long value;
+
   String sex;
   String reliabilty;
 }
